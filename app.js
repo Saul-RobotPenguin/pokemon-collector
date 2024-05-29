1ec0b0aa64cb0d17d40cf2972907c301885f8514
scalleja <scalleja12345@gmail.com>
2024-05-29 12:09:32 -0400

    Added links and beginning to add elements through js

---
+++ app.js
@@ -0,0 +1,71 @@
+const pokemon = [
+    {
+        name: "Pikachu",
+        subname: "The Rat Pokemon",
+        description: "Pikachu is an Electric Type",
+        image : "https://i.ebayimg.com/images/g/B~gAAOSwhNthhdjn/s-l1600.webp"
+    },
+
+    {
+        name: "Osshawott",
+        subname: "The Otter Pokemon",
+        description: "Osshawott is a Water Type",
+        image : "https://i.ebayimg.com/images/g/B~gAAOSwhNthhdjn/s-l1600.webp"
+    },
+    {
+        name: "Pikachu",
+        subname: "The Rat Pokemon",
+        description: "Pikachu is an Electric Type",
+        image : "https://i.ebayimg.com/images/g/B~gAAOSwhNthhdjn/s-l1600.webp"
+    },
+
+    {
+        name: "Osshawott",
+        subname: "The Otter Pokemon",
+        description: "Osshawott is a Water Type",
+        image : "https://i.ebayimg.com/images/g/B~gAAOSwhNthhdjn/s-l1600.webp"
+    },
+    {
+        name: "Pikachu",
+        subname: "The Rat Pokemon",
+        description: "Pikachu is an Electric Type",
+        image : "https://i.ebayimg.com/images/g/B~gAAOSwhNthhdjn/s-l1600.webp"
+    },
+
+    {
+        name: "Osshawott",
+        subname: "The Otter Pokemon",
+        description: "Osshawott is a Water Type",
+        image : "https://i.ebayimg.com/images/g/B~gAAOSwhNthhdjn/s-l1600.webp"
+    }
+]
+
+console.log(pokemon)
+
+const container = document.getElementById("container")
+
+for (let i = 0; i < pokemon.length; i++){
+    let column = document.createElement("div")
+    column.classList.add("column")
+    container.append(column)
+
+//1st Part of Card
+    let uiCard = document.createElement("div")
+   uiCard.className = "ui card"
+    column.append(uiCard)
+
+    let imageContainer = document.createElement("div")
+    imageContainer.className = "img"
+    uiCard.appendChild(imageContainer)
+
+    let image = document.createElement("img")
+    image.src =pokemon[i].image
+    imageContainer.appendChild(image)
+//2nd Part Of Cards
+
+    let content = document.createElement("div")
+    content.classList.add("content")
+    column.appendChild(content)
+
+
+}
